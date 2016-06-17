6ff10e9d6ccb575ec0e2dc4d8abff535650b7873
Gaetano Pessa <nomeecognome@gmail.com>
2016-06-17 16:07:22 +0200

    test

--- client/components/containers/simple/simple.directive.js
+++ client/components/containers/simple/simple.directive.js
@@ -13,14 +13,49 @@ angular
           scope.item = item;
           scope.editMode = editMode;
 
-          scope.inserted = function(event, index, item, oldparent){
-            console.log('inserted ' + scope.item.model._id)
+          scope.inserted = function(event, item, oldparent){
+            console.log('inserted ')
+            console.log(event, scope.item.model._id);
             scope.item.save();
           }
 
-          scope.remove = function(event, index, child, oldparent){
+          scope.remove = function(event, child, oldparent){
             console.log('callback ' + scope.item.model._id + ' ' + oldparent._id)
           }
+
+
+          scope.inserted = function(event, index, item){
+            console.log('inserted');
+            console.log(event, scope.item.model._id);
+            // scope.item.model.children.push(item);
+            scope.item.save();
+            return item;
+          }
+          scope.drop = function(event, item){
+            console.log('drop');
+            // console.log(event, scope.item.model._id);
+            // scope.item.model.children.push(item);
+            scope.item.save();
+            return item;
+          }
+
+          scope.moved = function(event){
+            console.log('moved');
+            console.log(event, scope.item.model._id);
+          }
+
+          scope.copied = function(event){
+            console.log('copied');
+            console.log(event, scope.item.model._id);
+          }
+
+          scope.dragend = function(event, item){
+            console.log('dragend');
+            console.log(event, scope.item.model._id);
+            scope.item.model.children.remove(item);
+            scope.item.save();
+          }
+
         }
       }
 
