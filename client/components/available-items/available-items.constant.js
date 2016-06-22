ee6b88352c9857440718f978481b934ab2e53122
gpessa <nomeecognome@gmail.com>
2016-06-22 23:36:39 +0200

    fix tab

--- client/components/available-items/available-items.constant.js
+++ client/components/available-items/available-items.constant.js
@@ -1,86 +1,85 @@
 'use strict';
-
 angular
   .module('gpAppApp')
   .constant('availableItems', {
     'Widget': [{
-        'icon': 'fa fa-list',
-        'type': 'widget',
-        'subtype': 'todo-list',
-        'attributes': {
-          'name': 'Todo List'
-        }
-      },{
-        'icon': 'icon icon-cigarette',
-        'type': 'widget',
-        'subtype': 'cigarette',
-        'attributes': {
-          'name': 'Cigarette'
-        }
-      },{
-        'icon': 'fa fa-line-chart',
-        'type': 'widget',
-        'subtype': 'withings',
-        'attributes': {
-          'name': 'Withings'
-        }
-      },{
-        'icon': 'fa fa-sun-o',
-        'type': 'widget',
-        'subtype': 'buienradar',
-        'attributes': {
-          'name': 'Buienradar'
-        }
-      },{
-        'icon': 'fa fa-university',
-        'type': 'widget',
-        'subtype': 'stock',
-        'attributes': {
-          'name': 'Stocks'
-        }
-      },{
-        'icon': 'fa fa-exchange',
-        'type': 'widget',
-        'subtype': 'portfolio',
-        'attributes': {
-          'name': 'Portfolio'
-        }
-      },{
-        'icon': 'fa fa-line-chart',
-        'type': 'widget',
-        'subtype': 'balance',
-        'attributes': {
-          'name': 'Balance'
-        }
-      },{
-        'icon': 'fa fa-plus',
-        'type': 'widget',
-        'subtype': 'positive-thing',
-        'attributes': {
-          'name': 'Positive Thing'
-        }
-      },{
-        'icon': 'fa fa-sun-o',
-        'type': 'widget',
-        'subtype': 'forecast',
-        'attributes': {
-          'name': 'Forecast'
-        }
+      'icon': 'fa fa-list',
+      'type': 'widget',
+      'subtype': 'todo-list',
+      'attributes': {
+        'name': 'Todo List'
+      }
+    }, {
+      'icon': 'icon icon-cigarette',
+      'type': 'widget',
+      'subtype': 'cigarette',
+      'attributes': {
+        'name': 'Cigarette'
+      }
+    }, {
+      'icon': 'fa fa-line-chart',
+      'type': 'widget',
+      'subtype': 'withings',
+      'attributes': {
+        'name': 'Withings'
+      }
+    }, {
+      'icon': 'fa fa-sun-o',
+      'type': 'widget',
+      'subtype': 'buienradar',
+      'attributes': {
+        'name': 'Buienradar'
+      }
+    }, {
+      'icon': 'fa fa-university',
+      'type': 'widget',
+      'subtype': 'stock',
+      'attributes': {
+        'name': 'Stocks'
+      }
+    }, {
+      'icon': 'fa fa-exchange',
+      'type': 'widget',
+      'subtype': 'portfolio',
+      'attributes': {
+        'name': 'Portfolio'
+      }
+    }, {
+      'icon': 'fa fa-line-chart',
+      'type': 'widget',
+      'subtype': 'balance',
+      'attributes': {
+        'name': 'Balance'
+      }
+    }, {
+      'icon': 'fa fa-plus',
+      'type': 'widget',
+      'subtype': 'positive-thing',
+      'attributes': {
+        'name': 'Positive Thing'
+      }
+    }, {
+      'icon': 'fa fa-sun-o',
+      'type': 'widget',
+      'subtype': 'forecast',
+      'attributes': {
+        'name': 'Forecast'
+      }
     }],
     'Wrapper': [{
-        'hidden': true,
-        'icon': 'fa fa-square-o',
-        'type': 'wrapper',
-        'subtype': 'simple',
-        'attributes': {
-          'name': 'Simple'
-        }
-      },{
-        'icon': 'fa fa-columns',
-        'type': 'wrapper',
-        'subtype': 'columns',
-        'attributes': {
-          'name': 'Columns'
-        }
+      'hidden': true,
+      'icon': 'fa fa-square-o',
+      'type': 'wrapper',
+      'subtype': 'simple',
+      'attributes': {
+        'name': 'Simple'
+      }
+    }, {
+      'icon': 'fa fa-columns',
+      'type': 'wrapper',
+      'subtype': 'columns',
+      'attributes': {
+        'name': 'Columns'
+      }
     }]
   });
