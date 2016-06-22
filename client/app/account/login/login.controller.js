4b5c615d685d99dbb5f60303f6b97b964c4bade8
gpessa <nomeecognome@gmail.com>
2016-06-22 23:33:14 +0200

    style login and signup

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -1,18 +1,15 @@
 'use strict';
 
 class LoginController {
+
   constructor(Auth, $state) {
-    this.user = {};
     this.errors = {};
-    this.submitted = false;
 
     this.Auth = Auth;
     this.$state = $state;
   }
 
   login(form) {
-    this.submitted = true;
-
     if (form.$valid) {
       this.Auth.login({
           email: this.user.email,
@@ -27,6 +24,7 @@ class LoginController {
         });
     }
   }
+  
 }
 
 angular.module('gpAppApp')
